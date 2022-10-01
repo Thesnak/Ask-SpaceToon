163149729ae1d96f23830c54645fefab29f67cff
FarahAllam <92639548+FarahAllam@users.noreply.github.com>
2022-10-01 11:31:44 +0200

    Update search_details.dart

--- spaceapp/lib/modules/search_details.dart
+++ spaceapp/lib/modules/search_details.dart
@@ -43,10 +43,9 @@ class ScreenDetails extends StatelessWidget {
                   ),
                   FlutterImageStack(
                     imageList: [
-                      'https://images.unsplash.com/photo-1593642532842-98d0fd5ebc1a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80',
-                      'https://images.unsplash.com/photo-1612594305265-86300a9a5b5b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-                      'https://images.unsplash.com/photo-1612626256634-991e6e977fc1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1712&q=80',
-                      'https://images.unsplash.com/photo-1593642702749-b7d2a804fbcf?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80'
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/1200px-OSIRIS_Mars_true_color.jpg',
+                      'https://cdn.mos.cms.futurecdn.net/BiH44Z2Wd9PS55wXBuQK8H.jpg'
+                      //'https://www.worldatlas.com/upload/bb/c3/32/shutterstock-1041249343.jpg'
 
                     ],
 
